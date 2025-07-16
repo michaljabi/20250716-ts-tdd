1c291143ef9eca204168526217f79aa07f43f636
Jabi <michal.jabi@gmail.com>
2025-07-16 09:53:21 +0200

    explain(hello-world): TS type inference

--- ts-tdd-live-theory/src/0-warm-up/hello-world.ts
+++ ts-tdd-live-theory/src/0-warm-up/hello-world.ts
@@ -1,2 +1,11 @@
 console.log('Hello World!', 2 + 20, 100 + '!', 4000);
 
+const myNumber = 10;
+
+let myOtherNumber = 100;
+
+// myOtherNumber = '?'
+
+type Money = typeof myOtherNumber;
+
+let ok: Money = 11;
