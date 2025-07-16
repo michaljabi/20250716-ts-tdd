ea29b4191e7d5bd251d2262018c794b1a0dcf608
Jabi <michal.jabi@gmail.com>
2025-07-16 10:45:14 +0200

    explain(3): how to run `.ts` files + `tsconfig.json` explain (initial)

--- ts-tdd-live-theory/src/0-warm-up/hello-world.ts
+++ ts-tdd-live-theory/src/0-warm-up/hello-world.ts
@@ -1,11 +1,24 @@
 console.log('Hello World!', 2 + 20, 100 + '!', 4000);
 
-const myNumber = 10;
+let myNumber = 10n;
 
 let myOtherNumber = 100;
+
+[2,3,4].at(-1);
 
 // myOtherNumber = '?'
 
 type Money = typeof myOtherNumber;
 
 let ok: Money = 11;
+
+// 1. sposoby na uruchomienie
+// npx ts-node .\hello-world.ts
+// npx tsx .\hello-world.ts
+
+// 2. node 24 (experimental)
+// node .\hello-world.ts
+
+// 3. npm run build + przejście do `/dist` oraz:
+// node .\hello-world.js
+
