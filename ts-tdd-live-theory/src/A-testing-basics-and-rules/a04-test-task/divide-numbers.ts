96564b0ca8b346a9bd1ecf65baf654ed53e2c7ae
Jabi <michal.jabi@gmail.com>
2025-07-16 14:13:19 +0200

    task(a04.2)|[REFACTOR]: step 3 - think about your code.

--- ts-tdd-live-theory/src/A-testing-basics-and-rules/a04-test-task/divide-numbers.ts
+++ ts-tdd-live-theory/src/A-testing-basics-and-rules/a04-test-task/divide-numbers.ts
@@ -1,7 +1,16 @@
-export function divideNumbers(a: number, b: number): number {
-    if(Number.isNaN(a) || Number.isNaN(b)) {
+function throwIfNaN(num: number) {
+    if(Number.isNaN(num)) {
         throw new Error('number cannot be NaN!');
     }
+}
+
+
+export function divideNumbers(a: number, b: number): number {
+    // if(Number.isNaN(a) || Number.isNaN(b)) {
+    //     throw new Error('number cannot be NaN!');
+    // }
+    throwIfNaN(b);
+    throwIfNaN(a);
     if(b === 0) {
         throw new Error('Cannot divide by 0!');
     }
