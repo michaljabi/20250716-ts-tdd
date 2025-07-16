d80a4257b6e6d435c6f55cb43d873fab23f5e293
Jabi <michal.jabi@gmail.com>
2025-07-16 11:00:50 +0200

    task(4): TS in new project + node.js + ESM (Ecma Script Modules) vs CommonJS (require/module.export)

---
+++ ts-tdd-live-theory/ts-project/hello.js
@@ -0,0 +1,4 @@
+// import { test } from 'vitest'
+export let x = 1000;
+// test.todo('Planuje test...');
+console.log(x);
---
+++ ts-tdd-live-theory/ts-project/hello.ts
@@ -0,0 +1,7 @@
+// import { test } from 'vitest'
+
+export let x: number = 1000;
+
+// test.todo('Planuje test...');
+
+console.log(x);
