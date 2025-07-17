d83727c6c8c10b0485fb9afd1deae7613a548c11
Jabi <michal.jabi@gmail.com>
2025-07-17 14:25:57 +0200

    task(RPS): done step 3 - test plan made. For game and game-state.

---
+++ tdd-project/src/game/rps-game-state.spec.ts
@@ -0,0 +1,8 @@
+    describe('game state', () => {
+        it.each([
+
+        ])('should display proper game state interpretation [RQ-7]', () => {})
+
+
+        it.todo('should track the game state, win/ loose / draw count for EndUser [RQ-8]', () => {})
+    })
---
+++ tdd-project/src/game/rps-game.spec.ts
@@ -0,0 +1,24 @@
+import { it, expect, describe } from 'vitest'
+
+
+describe('RPS game', () => {
+   
+    
+    it.todo('should have 3 choices rock, paper, scissors in game [RQ-1, RQ-4]');
+
+
+    it.each([
+
+    ])('should result in game state `DRAW` if both player have same choices [RQ-5, RQ-6]', () => {})
+
+    it.each([
+
+    ])('should result in game state `WIN` if 1st player choice is %s and 2nd player choice is %s [RQ-5, RQ-6]', () => {})
+
+    it.each([
+
+    ])('should result in game state `LOOSE` if 1st player choice is %s and 2nd player choice is %s [RQ-5, RQ-6]', () => {})
+
+
+
+})
